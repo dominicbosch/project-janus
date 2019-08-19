15fdd985e8bd61b6d14549113c9e42146883bf8a
Dominic Bosch <dominic.bosch@bayer.com>
2019-08-19 10:08:55 +0200

    debug

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const RoboControl = require('./robocontrol');
 const rob = new RoboControl();
 
 rob.left()
-    .then(rob.right())
+    .then(() => rob.right())
     .then(() => {
         console.log('exiting');
         rob.exit();
