f90834487d4d2e5fcf7cc17ea6cf7c6ed618df4f
Dominic Bosch <dominic.bosch.db@gmail.com>
2019-08-23 23:44:01 +0200

    debugging

--- node-robo/robo.js
+++ node-robo/robo.js
@@ -28,24 +28,31 @@ module.exports = function(RED) {
                     } else {
                         currY = parseFloat(data.value);
                     }
+                    msg.payload.handler = 'MOVE x='+currX+', y='+currY;
                     handleRoboResult(node, msg, rob.steer(currX, currY));
                 } else if (data.action === 'arm' && data.value) {
                     if (data.value === 'stopped') {
+                        msg.payload.handler = 'ARM STOP';
                         handleRoboResult(node, msg, rob.armStop());
                     } else {
                         if (data.direction === 'top') {
+                            msg.payload.handler = 'ARM UP';
                             handleRoboResult(node, msg, rob.armUp());
                         } else {
+                            msg.payload.handler = 'ARM DOWN';
                             handleRoboResult(node, msg, rob.armDown());
                         }
                     }
                 } else if (data.action === 'gripper' && data.value) {
                     if (data.value === 'stopped') {
+                        msg.payload.handler = 'GRIPPER STOP';
                         handleRoboResult(node, msg, rob.gripperStop());
                     } else {
                         if (data.direction === 'open') {
+                            msg.payload.handler = 'GRIPPER OPEN';
                             handleRoboResult(node, msg, rob.gripperOpen());
                         } else {
+                            msg.payload.handler = 'GRIPPER CLOSE';
                             handleRoboResult(node, msg, rob.gripperClose());
                         }
                     }
