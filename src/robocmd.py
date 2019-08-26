c1b88c3baf862c8927a6b3f2dabfe70ff2967235
Dominic Bosch <dominic.bosch@bayer.com>
2019-08-26 16:34:51 +0200

    bugfixing exiting python

--- src/robocmd.py
+++ src/robocmd.py
@@ -1,3 +1,5 @@
+import time
+import sys
 from megapi import *
 
 print("Bot init")
@@ -7,14 +9,16 @@ print("Bot start")
 bot.start()
 
 print("Bot listens")
-for line in sys.stdin:
-    print("Bot got command '{}'".format(line))
-    arr = line.split(',')
-    cmdid = arr[0]
-    motor = int(arr[1])
-    val = float(arr[2])
-    if cmdid is not None and motor is not None and val is not None:
-        bot.encoderMotorRun(motor, int(val*255))
-        print('DONE,{}'.format(cmdid))
-    else:
-        print("Error!")
+while True:
+    time.sleep(0.01)
+    for line in sys.stdin:
+        print("Bot got command '{}'".format(line))
+        arr = line.split(',')
+        cmdid = arr[0]
+        motor = int(arr[1])
+        val = float(arr[2])
+        if cmdid is not None and motor is not None and val is not None:
+            bot.encoderMotorRun(motor, int(val*255))
+            print('DONE,{}'.format(cmdid))
+        else:
+            print("Error!")
