76086161d626172a0c2a4a837cd52cdd594fc78a
Dominic Bosch <dominic.bosch@bayer.com>
2019-08-27 18:20:09 +0200

    manual ride python

--- src/robocmd_cmdline.py
+++ src/robocmd_cmdline.py
@@ -1,3 +1,4 @@
+import click
 from megapi import *
 
 print("Bot init")
@@ -7,19 +8,86 @@ print("Bot start")
 bot.start()
 
 print("Bot listens")
-
 while True:
-    text = input("prompt")  # Python 3
-    print(text)
+    c  = click.getchar()
+    click.echo()
+    if c == 's':
+        click.echo('STOP ALL!')
+        stopAll()
+    elif c == 'q':
+        click.echo('Abort!')
+        stopAll()
+        break
+    elif c == '\x1b[D':
+        click.echo('LEFT')
+        left()
+    elif c == '\x1b[C':
+        click.echo('RIGHT')
+        right()
+    elif c == '\x1b[A':
+        click.echo('FORWARD')
+        forward()
+    elif c == '\x1b[B':
+        click.echo('BACKWARD')
+        backward()
+    elif c == 'u':
+        click.echo('ARM UP')
+        armUp()
+    elif c == 'd':
+        click.echo('ARM DOWN')
+        armDown()
+    elif c == 'o':
+        click.echo('GRIPPER OPEN')
+        gripperOpen()
+    elif c == 'c':
+        click.echo('GRIPPER CLOSE')
+        gripperClose()
 
-    # for line in sys.stdin:
-    #     print("Bot got command '{}'".format(line))
-    #     arr = line.split(',')
-    #     cmdid = arr[0]
-    #     motor = int(arr[1])
-    #     val = float(arr[2])
-    #     if cmdid is not None and motor is not None and val is not None:
-    #         bot.encoderMotorRun(motor, int(val*255))
-    #         print('DONE,{}'.format(cmdid))
-    #     else:
-    #         print("Error!")
+def left():
+    bot.encoderMotorRun(1, 30)
+    bot.encoderMotorRun(2, 30)
+
+def right():
+    bot.encoderMotorRun(1, -30)
+    bot.encoderMotorRun(2, -30)
+
+def forward():
+    bot.encoderMotorRun(2, -150)
+    bot.encoderMotorRun(1, 150)
+
+def backward():
+    bot.encoderMotorRun(2, 150)
+    bot.encoderMotorRun(1, -150)
+
+def armUp():
+    bot.encoderMotorRun(3, -76)
+
+def armDown():
+    bot.encoderMotorRun(3, 76)
+
+def armStop():
+    bot.encoderMotorRun(3, 0)
+
+def gripperOpen():
+    bot.encoderMotorRun(4, 150)
+
+def gripperClose():
+    bot.encoderMotorRun(4, -20)
+
+def gripperStop():
+    bot.encoderMotorRun(4, 0)
+
+def stopAll():
+    stopMove()
+    stopArm()
+    stopGripper()
+
+def stopMove():
+    bot.encoderMotorRun(1, 0)
+    bot.encoderMotorRun(2, 0)
+
+def stopArm():
+    bot.encoderMotorRun(3, 0)
+
+def stopGripper():
+    bot.encoderMotorRun(4, 0)
